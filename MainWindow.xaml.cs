28a177cbda76841ff07aa61f9cba181649884053
Pham Ngoc Vinh <vinhgalaxys2@gmail.com>
2021-03-29 21:33:01 +0900

    Initialize Commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -23,6 +25,29 @@ namespace automatic_engine
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            var folderDialog = new FolderBrowserDialog();
+            DialogResult dialogResult = folderDialog.ShowDialog();
+            if (System.Windows.Forms.DialogResult.OK.Equals(dialogResult))
+            {
+                TxtPath.Text = folderDialog.SelectedPath;
+            }
+        }
+
+        private void BtnExecute_Click(object sender, RoutedEventArgs e)
+        {
+            var strPath = TxtPath.Text;
+            if (!string.IsNullOrEmpty(strPath))
+            {
+                var arrFiles = Directory.GetFiles(strPath);
+                foreach (string filePath in arrFiles)
+                {
+
+                }
+            }
         }
     }
 }
